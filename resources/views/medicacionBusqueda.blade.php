a094295231b4cfd9ea932ca47b33593d0fa4434c
Axel Cerkvenih <cerkvenihaxel@gmail.com>
2023-03-29 16:29:17 -0300

    Agregado icono

--- resources/views/medicacionBusqueda.blade.php
+++ resources/views/medicacionBusqueda.blade.php
@@ -4,6 +4,9 @@
         <meta charset="utf-8" lang="es">
         <title id="titulo">Siscon Medicacion</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="icon" href="/SISCON.png" type="image/x-icon">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <!-- Font Awesome CDN -->
@@ -102,7 +105,13 @@
 
             </tbody>
         </table>
-    </div>
 
+
+    </div>
+        <div class="row">
+            <div class="col text-center mt-3">
+                <button class="btn btn-primary" onclick="window.print()">Imprimir</button>
+            </div>
+        </div>
     </body>
 
