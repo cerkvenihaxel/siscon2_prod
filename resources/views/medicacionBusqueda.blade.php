5bfa9539aada24d30c688d3936aabdcb4230e55b
Axel Cerkvenih <cerkvenihaxel@gmail.com>
2023-03-30 00:35:03 -0300

    Boton Kairoz

--- resources/views/medicacionBusqueda.blade.php
+++ resources/views/medicacionBusqueda.blade.php
@@ -99,8 +99,8 @@
                     <td>{{ DB::table('articulosZafiro')->where('id', $pD->articuloZafiro_id)->value('des_articulo') }} || {{ DB::table('articulosZafiro')->where('id', $pD->articuloZafiro_id)->value('presentacion') }} </td>
                     <td>{{$pD->cantidad}}</td>
                     <td>
-                        <a href="https://ar.kairosweb.com/?s={{$monodroga}}">
-                            LINK
+                        <a href="https://ar.kairosweb.com/?s={{$monodroga}}" class="boton-kairoz" target="_blank">
+                            Consultar
                         </a>
                     </td>
                     </tr>
@@ -118,5 +118,34 @@
         </div>
 
     </div>
+
     </body>
 
+
+<style>
+    .boton-kairoz {
+        display: inline-block;
+        font-weight: 400;
+        color: #fff;
+        text-align: center;
+        vertical-align: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+        background-color: #28a745;
+        border: 1px solid #28a745;
+        padding: 0.375rem 0.75rem;
+        font-size: 1rem;
+        line-height: 1.5;
+        border-radius: 0.25rem;
+        transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+    }
+
+    .boton-kairoz:hover {
+        color: #fff;
+        background-color: #218838;
+        border-color: #1e7e34;
+    }
+
+</style>
