09c39f30691a91b1acfd52dc2fcafc1f39fb3975
cerkvenihaxel <cerkvenihaxel@gmail.com>
2023-07-12 15:20:22 -0300

    fix 'Convenio Cotizacion imprimir fix detalle ok'

--- resources/views/pdf/tabla.blade.php
+++ resources/views/pdf/tabla.blade.php
@@ -80,11 +80,7 @@
         @foreach($detalles as $key => $detalle)
             <tr>
                 <td>{{ $key +1 }}</td>
-                @php
-                    $articuloID = $detalle->articuloZafiro_id;
-                    $numeroArticulo = str_pad($articuloID, 10, '0', STR_PAD_LEFT); // Rellena con ceros a la izquierda
-                @endphp
-                <td>{{ DB::table('articulosZafiro')->where('id_articulo', $numeroArticulo)->value('presentacion_completa') }}</td>
+                <td>{{ DB::table('articulosZafiro')->where('id', $detalle->articuloZafiro_id)->value('des_articulo') }} | {{DB::table('articulosZafiro')->where('id', $detalle->articuloZafiro_id)->value('presentacion')}}</td>
                 <td>{{ $detalle->cantidad }}</td>
                 <td>{{$detalle->total}}</td>
                 <!-- Agrega aquí más celdas según los datos que desees mostrar de $detalles -->
