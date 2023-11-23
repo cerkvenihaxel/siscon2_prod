afc771536725ebbcea216cfceb34e96644744dc0
Facundo Nieto <facundonieto24@gmail.com>
2023-11-23 15:54:45 -0300

    feat(ReportesGenerales): Add date interval

--- app/Http/Controllers/ReportesGenerales.php
+++ app/Http/Controllers/ReportesGenerales.php
@@ -11,6 +11,7 @@ use App\Exports\ReporteSinCotizar;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Excel;
+use Carbon\Carbon;
 
 
 class ReportesGenerales extends Controller
@@ -20,129 +21,135 @@ class ReportesGenerales extends Controller
         return view('reportesgrales.index');
     }
 
-    public function reporteProveedoresExcel(){
+    public function reporteProveedoresExcel(Request $request){
 
+		//recibo las fechas, desde el método HTTP POST tomado del formulario:
+		$dateRange = $request->input('daterange');
+		list($start_date, $end_date) = explode(' - ', $dateRange);
+		$start_date = Carbon::createFromFormat('Y-m-d', $start_date)->toDateString();
+		$end_date = Carbon::createFromFormat('Y-m-d', $end_date)->toDateString();
 
-// Configura la conexión a la base de datos en el archivo .env o en el archivo de configuración de Laravel (config/database.php)
-$connection = config('database.default'); // Esto asumirá la conexión predeterminada definida en config/database.php
+		// Configura la conexión a la base de datos en el archivo .env o en el archivo de configuración de Laravel (config/database.php)
+		$connection = config('database.default'); // Esto asumirá la conexión predeterminada definida en config/database.php
 
 // Realiza la consulta utilizando Eloquent
-$resultados = DB::connection($connection)->table(
-        DB::raw('
-        (
-    SELECT "cotizaciones" AS tabla, created_at, proveedor AS sub_proveedor
-    FROM cotizaciones
-    WHERE YEAR(created_at) BETWEEN 2023 AND 2026 AND MONTH(created_at) BETWEEN 1 AND 12
-    UNION ALL
-    SELECT "adjudicaciones" AS tabla, ad.created_at, ad.adjudicatario AS sub_proveedor
-    FROM adjudicaciones ad
-	WHERE YEAR(ad.created_at) BETWEEN 2023 AND 2026 AND MONTH(ad.created_at) BETWEEN 1 AND 12
-    UNION ALL
-    SELECT "autorizaciones" AS tabla, au.created_at, au.autorizado AS sub_proveedor
-    FROM autorizaciones au
-    WHERE YEAR(au.created_at) BETWEEN 2023 AND 2026 AND MONTH(au.created_at) BETWEEN 1 AND 12
-) AS subconsulta'))
-    ->selectRaw('
-    CASE 
-		WHEN sub_proveedor IN ("Angiocor") THEN "ANGIOCOR"
-		WHEN sub_proveedor IN ("Artro", "ARTRO S.A") THEN "ARTRO S.A"
-		WHEN sub_proveedor IN ("Amplitone") THEN "AMPLITONE"
-        WHEN sub_proveedor IN ("Aved") THEN "AVED"
-        WHEN sub_proveedor IN ("BERNAT.", "Ortopedia Bernat", "ORTOPEDIA BERNAT.", "BERNAT") THEN "ORTOPEDIA BERNAT"
-		WHEN sub_proveedor IN ("Biolap") THEN "BIOLAP"
-        WHEN sub_proveedor IN ("Biolatina") THEN "BIOLATINA"
-        WHEN sub_proveedor IN ("Bionor") THEN "BIONOR"
-        WHEN sub_proveedor IN ("CARDIO -PACK","CardioPack") THEN "CARDIO-PACK"
-        WHEN sub_proveedor IN ("CARE QUIP","CARE-QUIP") THEN "CARE QUIP."
-		WHEN sub_proveedor IN ("CareMedical","CarMedical", "CAR MEDICAL") THEN "CARE MEDICAL"
-		WHEN sub_proveedor IN ("CHEMA","ORTOPEDIA CHEMA.") THEN "ORTOPEDIA CHEMA"
-		WHEN sub_proveedor IN ("CHIAVASSA.") THEN "ORTOPEDIA CHIAVASSA"
-		WHEN sub_proveedor IN ("CONSELGI WIDEX") THEN "WIDEX"
-		-- DEBENE SA se cargó bien
-		WHEN sub_proveedor IN ("DECADE.", "Decade") THEN "DECADE"
-		WHEN sub_proveedor IN ("DOCTOR PIE.","DR PIE","DR PIE.","PROPIEDAD DR PIE.") THEN "DOCTOR PIE"
-		-- DrGLOBAL MEDICA quedó en rojo en el excel de sol, hay que borrarlo?
-		-- ELVIRA se cargó bien
-		-- ENDOVIA se cargó bien
-		-- EXACTECH se elimina? está en rojo en el excel de sol
-		WHEN sub_proveedor IN ("Forca", "Forca SRL","FORCA.") THEN "FORCA"
-		-- FORUM se cargó bien
-		WHEN sub_proveedor IN ("Global Medica","Global Medica  S.A.","Global Medica S.A","Global Medica S.A.","GLOBAL MEDICA.","GLOBAL MEDICAL","GM","GM SA","GMSA") THEN "GLOBAL MEDICA S.A."
-		-- GMR S.A. ? lo dejé así
-		WHEN sub_proveedor IN ("GD.BIO","GS  BIO.","GS BIO","GS BIO.","GS-BIO","GS.BIO","GSBIO","Gsbio") THEN "GS-BIO"
-		WHEN sub_proveedor IN ("GM","GM SA","GMR S.A.","GMSA") THEN "GMRS"
-		WHEN sub_proveedor IN ("IGMA.", "Igma") THEN "IGMA"
-		WHEN sub_proveedor IN ("IM Salud") THEN "IM SALUD"
-		-- IMECO se cargó bien
-		WHEN sub_proveedor IN ("IMPLACOR.", "Implacor") THEN "IMPLACOR"
-		WHEN sub_proveedor IN ("Implantes Medicos", "Implantes Medicos - cotización sin tornillos solicitados") THEN "IMPLANTES MEDICOS"
-		WHEN sub_proveedor IN ("IMTERMED","INTERMED", "Intermed Rehab","INTERMED.") THEN "INTERMED."
-		WHEN sub_proveedor IN ("INTERSIL.") THEN "INTERSIL"
-		WHEN sub_proveedor IN ("Ip Magna") THEN "IP MAGNA"
-		WHEN sub_proveedor IN ("Kineret") THEN "KINERET"
-		WHEN sub_proveedor IN ("Mat Medical") THEN "MAT MEDICAL"
-		-- MC MEDICAL se cargó bien
-		WHEN sub_proveedor IN ("Medcare") THEN "MED CARE"
-		WHEN sub_proveedor IN ("Medel","MEDEL") THEN "MED-EL"
-		WHEN sub_proveedor IN ("Medical Implants") THEN "MEDICAL IMPLANTS"
-		WHEN sub_proveedor IN ("Medical Milenium") THEN "MEDICAL MILENIUM"
-		WHEN sub_proveedor IN ("Medical Supplies") THEN "MEDICAL SUPPLIES"
-		WHEN sub_proveedor IN ("Medical Team", "Medical Team SIN S") THEN "MEDICAL TEAM"
-		WHEN sub_proveedor IN ("MEDKIT") THEN "MEDKIT SRL"
-		WHEN sub_proveedor IN ("Medpro") THEN "MEDPRO"
-		WHEN sub_proveedor IN ("Miguel Angel", "ORT. MIGUEL ANGEL","Ortopedia Miguel Angel") THEN "ORTOPEDIA MIGUEL ANGEL"
-		-- MT? qué hacer? se deja como está o se elimina? 
-		WHEN sub_proveedor IN ("Mobility") THEN "MOBILITY"
-		WHEN sub_proveedor IN ("Nexo") THEN "NEXO"
-		WHEN sub_proveedor IN ("North Medical") THEN "NORTH MEDICAL"
-		WHEN sub_proveedor IN ("Nova","Nova Soluciones Quirúrgicas","Nova Soluciones Quirurgicas", "Nova Soluciones Quirugicas", "Nova Soluciones") THEN "NOVA SOLUCIONES" 
-		WHEN sub_proveedor IN ("Nowa", "Nowa Portesis", "Nowa Protesis", "Nowa Protesis", "Nowakowski Maria Clara") THEN "NOWA"
-		-- OLYMPIA está bien cargado
-		WHEN sub_proveedor IN ("Omicron") THEN "OMICRON"
-		WHEN sub_proveedor IN ("Ortopedia Mayo") THEN "ORTOPEDIA MAYO"
-		WHEN sub_proveedor IN ("Ortopedia Rapalar", "RAPALAR") THEN "ORTOPEDIA RAPALAR"
-		WHEN sub_proveedor IN ("Santa Lucia", "SANTA LUCIA.", "SANTA LUCIA", "Sta Lucia") THEN "ORTOPEDIA SANTA LUCIA"
-		WHEN sub_proveedor IN ("OSTEORIESTRA.", "Osteoriestra") THEN "OSTEORIESTRA"
-		-- PFM? qué es?
-		WHEN sub_proveedor IN ("PROMEDICAL.") THEN "PROMEDICAL"
-		WHEN sub_proveedor IN ("PROMEDON.") THEN "PROMEDON"
-		-- sub_proveedor prueba, sub_proveedorDOS, sub_proveedorTRES, sub_proveedorUNO, Prueba deberían borrarse no?
-		WHEN sub_proveedor IN ("QRA INSUMOS") THEN "QRA"
-		WHEN sub_proveedor IN ("REHAVITA.") THEN "REHAVITA"
-		-- ROFREN está bien cargado
-		-- RS MEDICA ? lo dejo como está
-		-- SANTA LUCIA lo dejé como ORTOPEDIA SANTA LUCIA
-		WHEN sub_proveedor IN ("SbTorres", "SB Torres") THEN "SB TORRES"
-		WHEN sub_proveedor IN ("SILFAB.") THEN "SILFAB"
-		WHEN sub_proveedor IN ("Silmag") THEN "SILMAG"
-		WHEN sub_proveedor IN ("SURGICAL SUPLLY","SURGICAL SUPPLY","SURGICAL SUPPLY.","SURGICALSUPPLY") THEN "SURGICAL SUPPLY"
-		WHEN sub_proveedor IN ("SURMESH.") THEN "SURMESH"
-		-- SWIPRO está bien cargado
-		WHEN sub_proveedor IN ("TECHNO  HEALTH", "Techno Healt", "TECHNO HEALTH", "Tecnosalud","TECNOSALUD.", "TECHNO-HEAT") THEN "TECHNO HEALTH"
-		-- UNIFARMA está bien cargado
-		WHEN sub_proveedor IN ("Valmi") THEN "VALMI"
-        ELSE sub_proveedor
-    END AS Proveedor,
-	YEAR(subconsulta.created_at) AS Año,
-    CASE
-    	WHEN MONTH(subconsulta.created_at) = 1 THEN "Enero"
-		WHEN MONTH(subconsulta.created_at) = 2 THEN "Febrero"
-    	WHEN MONTH(subconsulta.created_at) = 3 THEN "Marzo"
-		WHEN MONTH(subconsulta.created_at) = 4 THEN "Abril"
-    	WHEN MONTH(subconsulta.created_at) = 5 THEN "Mayo"
-		WHEN MONTH(subconsulta.created_at) = 6 THEN "Junio"
-    	WHEN MONTH(subconsulta.created_at) = 7 THEN "Julio"
-		WHEN MONTH(subconsulta.created_at) = 8 THEN "Agosto"
-    	WHEN MONTH(subconsulta.created_at) = 9 THEN "Septiembre"
-		WHEN MONTH(subconsulta.created_at) = 10 THEN "Octubre"
-    	WHEN MONTH(subconsulta.created_at) = 11 THEN "Noviembre"
-		WHEN MONTH(subconsulta.created_at) = 12 THEN "Diciembre"
-    ELSE "Otros"
-    END AS Mes,
-    COUNT(CASE WHEN subconsulta.tabla = "cotizaciones" THEN 1 END) AS Cotizadas,
-    COUNT(CASE WHEN subconsulta.tabla = "adjudicaciones" THEN 1 END) AS Adjudicadas,
-    COUNT(CASE WHEN subconsulta.tabla = "autorizaciones" THEN 1 END) AS Finalizadas
-    ')->groupByRaw('Año, Proveedor, Mes')->orderByRaw('Proveedor, Mes, Año')->get();
+		$resultados = DB::connection($connection)->table(
+			DB::raw('
+			(
+				SELECT "cotizaciones" AS tabla, created_at, proveedor AS sub_proveedor
+				FROM cotizaciones
+				WHERE created_at BETWEEN "'.$start_date.'" AND "'.$end_date.'"
+				UNION ALL
+				SELECT "adjudicaciones" AS tabla, ad.created_at, ad.adjudicatario AS sub_proveedor
+				FROM adjudicaciones ad
+				WHERE created_at BETWEEN "'.$start_date.'" AND "'.$end_date.'" AND ad.estado_solicitud_id = 3 
+				UNION ALL
+				SELECT "autorizaciones" AS tabla, au.created_at, au.autorizado AS sub_proveedor
+				FROM autorizaciones au
+				WHERE created_at BETWEEN "'.$start_date.'" AND "'.$end_date.'"
+			) AS subconsulta'))
+			->selectRaw('
+				CASE 
+					WHEN sub_proveedor IN ("Angiocor") THEN "ANGIOCOR"
+					WHEN sub_proveedor IN ("Artro", "ARTRO S.A") THEN "ARTRO S.A"
+					WHEN sub_proveedor IN ("Amplitone") THEN "AMPLITONE"
+					WHEN sub_proveedor IN ("Aved") THEN "AVED"
+					WHEN sub_proveedor IN ("BERNAT.", "Ortopedia Bernat", "ORTOPEDIA BERNAT.", "BERNAT") THEN "ORTOPEDIA BERNAT"
+					WHEN sub_proveedor IN ("Biolap") THEN "BIOLAP"
+					WHEN sub_proveedor IN ("Biolatina") THEN "BIOLATINA"
+					WHEN sub_proveedor IN ("Bionor") THEN "BIONOR"
+					WHEN sub_proveedor IN ("CARDIO -PACK","CardioPack") THEN "CARDIO-PACK"
+					WHEN sub_proveedor IN ("CARE QUIP","CARE-QUIP") THEN "CARE QUIP."
+					WHEN sub_proveedor IN ("CareMedical","CarMedical", "CAR MEDICAL") THEN "CARE MEDICAL"
+					WHEN sub_proveedor IN ("CHEMA","ORTOPEDIA CHEMA.") THEN "ORTOPEDIA CHEMA"
+					WHEN sub_proveedor IN ("CHIAVASSA.") THEN "ORTOPEDIA CHIAVASSA"
+					WHEN sub_proveedor IN ("CONSELGI WIDEX") THEN "WIDEX"
+					-- DEBENE SA se cargó bien
+					WHEN sub_proveedor IN ("DECADE.", "Decade") THEN "DECADE"
+					WHEN sub_proveedor IN ("DOCTOR PIE.","DR PIE","DR PIE.","PROPIEDAD DR PIE.") THEN "DOCTOR PIE"
+					-- DrGLOBAL MEDICA quedó en rojo en el excel de sol, hay que borrarlo?
+					-- ELVIRA se cargó bien
+					-- ENDOVIA se cargó bien
+					-- EXACTECH se elimina? está en rojo en el excel de sol
+					WHEN sub_proveedor IN ("Forca", "Forca SRL","FORCA.") THEN "FORCA"
+					-- FORUM se cargó bien
+					WHEN sub_proveedor IN ("Global Medica","Global Medica  S.A.","Global Medica S.A","Global Medica S.A.","GLOBAL MEDICA.","GLOBAL MEDICAL","GM","GM SA","GMSA") THEN "GLOBAL MEDICA S.A."
+					-- GMR S.A. ? lo dejé así
+					WHEN sub_proveedor IN ("GD.BIO","GS  BIO.","GS BIO","GS BIO.","GS-BIO","GS.BIO","GSBIO","Gsbio") THEN "GS-BIO"
+					WHEN sub_proveedor IN ("GM","GM SA","GMR S.A.","GMSA") THEN "GMRS"
+					WHEN sub_proveedor IN ("IGMA.", "Igma") THEN "IGMA"
+					WHEN sub_proveedor IN ("IM Salud") THEN "IM SALUD"
+					-- IMECO se cargó bien
+					WHEN sub_proveedor IN ("IMPLACOR.", "Implacor") THEN "IMPLACOR"
+					WHEN sub_proveedor IN ("Implantes Medicos", "Implantes Medicos - cotización sin tornillos solicitados") THEN "IMPLANTES MEDICOS"
+					WHEN sub_proveedor IN ("IMTERMED","INTERMED", "Intermed Rehab","INTERMED.") THEN "INTERMED."
+					WHEN sub_proveedor IN ("INTERSIL.", "Intersil") THEN "INTERSIL"
+					WHEN sub_proveedor IN ("Ip Magna") THEN "IP MAGNA"
+					WHEN sub_proveedor IN ("Kineret") THEN "KINERET"
+					WHEN sub_proveedor IN ("Mat Medical") THEN "MAT MEDICAL"
+					-- MC MEDICAL se cargó bien
+					WHEN sub_proveedor IN ("Medcare") THEN "MED CARE"
+					WHEN sub_proveedor IN ("Medel","MEDEL") THEN "MED-EL"
+					WHEN sub_proveedor IN ("Medical Implants") THEN "MEDICAL IMPLANTS"
+					WHEN sub_proveedor IN ("Medical Milenium") THEN "MEDICAL MILENIUM"
+					WHEN sub_proveedor IN ("Medical Supplies") THEN "MEDICAL SUPPLIES"
+					WHEN sub_proveedor IN ("Medical Team", "Medical Team SIN S") THEN "MEDICAL TEAM"
+					WHEN sub_proveedor IN ("MEDKIT") THEN "MEDKIT SRL"
+					WHEN sub_proveedor IN ("Medpro") THEN "MEDPRO"
+					WHEN sub_proveedor IN ("Miguel Angel", "ORT. MIGUEL ANGEL","Ortopedia Miguel Angel") THEN "ORTOPEDIA MIGUEL ANGEL"
+					-- MT? qué hacer? se deja como está o se elimina? 
+					WHEN sub_proveedor IN ("Mobility") THEN "MOBILITY"
+					WHEN sub_proveedor IN ("Nexo") THEN "NEXO"
+					WHEN sub_proveedor IN ("North Medical") THEN "NORTH MEDICAL"
+					WHEN sub_proveedor IN ("Nova","Nova Soluciones Quirúrgicas","Nova Soluciones Quirurgicas", "Nova Soluciones Quirugicas", "Nova Soluciones") THEN "NOVA SOLUCIONES" 
+					WHEN sub_proveedor IN ("Nowa", "Nowa Portesis", "Nowa Protesis", "Nowa Protesis", "Nowakowski Maria Clara") THEN "NOWA"
+					-- OLYMPIA está bien cargado
+					WHEN sub_proveedor IN ("Omicron") THEN "OMICRON"
+					WHEN sub_proveedor IN ("Ortopedia Mayo") THEN "ORTOPEDIA MAYO"
+					WHEN sub_proveedor IN ("Ortopedia Rapalar", "RAPALAR") THEN "ORTOPEDIA RAPALAR"
+					WHEN sub_proveedor IN ("Santa Lucia", "SANTA LUCIA.", "SANTA LUCIA", "Sta Lucia") THEN "ORTOPEDIA SANTA LUCIA"
+					WHEN sub_proveedor IN ("Silfab") THEN "SILFAB"
+					WHEN sub_proveedor IN ("OSTEORIESTRA.", "Osteoriestra") THEN "OSTEORIESTRA"
+					-- PFM? qué es?
+					WHEN sub_proveedor IN ("PROMEDICAL.") THEN "PROMEDICAL"
+					WHEN sub_proveedor IN ("PROMEDON.", "Promedon") THEN "PROMEDON"
+					-- sub_proveedor prueba, sub_proveedorDOS, sub_proveedorTRES, sub_proveedorUNO, Prueba deberían borrarse no?
+					WHEN sub_proveedor IN ("QRA INSUMOS") THEN "QRA"
+					WHEN sub_proveedor IN ("REHAVITA.") THEN "REHAVITA"
+					-- ROFREN está bien cargado
+					-- RS MEDICA ? lo dejo como está
+					-- SANTA LUCIA lo dejé como ORTOPEDIA SANTA LUCIA
+					WHEN sub_proveedor IN ("SbTorres", "SB Torres") THEN "SB TORRES"
+					WHEN sub_proveedor IN ("SILFAB.") THEN "SILFAB"
+					WHEN sub_proveedor IN ("Silmag") THEN "SILMAG"
+					WHEN sub_proveedor IN ("SURGICAL SUPLLY","SURGICAL SUPPLY","SURGICAL SUPPLY.","SURGICALSUPPLY") THEN "SURGICAL SUPPLY"
+					WHEN sub_proveedor IN ("SURMESH.") THEN "SURMESH"
+					-- SWIPRO está bien cargado
+					WHEN sub_proveedor IN ("TECHNO  HEALTH", "Techno Healt", "TECHNO HEALTH", "Tecnosalud","TECNOSALUD.", "TECHNO-HEAT") THEN "TECHNO HEALTH"
+					-- UNIFARMA está bien cargado
+					WHEN sub_proveedor IN ("Valmi") THEN "VALMI"
+					ELSE sub_proveedor
+				END AS Proveedor,
+				YEAR(subconsulta.created_at) AS "Año",
+				CASE
+					WHEN MONTH(subconsulta.created_at) = 1 THEN "Enero"
+					WHEN MONTH(subconsulta.created_at) = 2 THEN "Febrero"
+					WHEN MONTH(subconsulta.created_at) = 3 THEN "Marzo"
+					WHEN MONTH(subconsulta.created_at) = 4 THEN "Abril"
+					WHEN MONTH(subconsulta.created_at) = 5 THEN "Mayo"
+					WHEN MONTH(subconsulta.created_at) = 6 THEN "Junio"
+					WHEN MONTH(subconsulta.created_at) = 7 THEN "Julio"
+					WHEN MONTH(subconsulta.created_at) = 8 THEN "Agosto"
+					WHEN MONTH(subconsulta.created_at) = 9 THEN "Septiembre"
+					WHEN MONTH(subconsulta.created_at) = 10 THEN "Octubre"
+					WHEN MONTH(subconsulta.created_at) = 11 THEN "Noviembre"
+					WHEN MONTH(subconsulta.created_at) = 12 THEN "Diciembre"
+				ELSE "Otros"
+				END AS Mes,
+				COUNT(CASE WHEN subconsulta.tabla = "cotizaciones" THEN 1 END) AS Cotizadas,
+				COUNT(CASE WHEN subconsulta.tabla = "adjudicaciones" THEN 1 END) AS Adjudicadas,
+				COUNT(CASE WHEN subconsulta.tabla = "autorizaciones" THEN 1 END) AS Finalizadas
+			')->groupByRaw('Año, Proveedor, Mes')->orderByRaw('Proveedor, Mes, Año')->get();
 
         return \Maatwebsite\Excel\Facades\Excel::download(new ReporteProveedoresExport($resultados), 'ReporteProveedorestado_solicitud.xlsx');
     }
