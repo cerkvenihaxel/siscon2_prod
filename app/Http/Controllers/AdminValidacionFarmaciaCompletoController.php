d9943b07b1e5e4d8f2f275cb4a26ab75c80bff2b
innovusvm <50964050+innovusvm@users.noreply.github.com>
2023-11-28 00:05:33 -0300

    feat: Farmacia Validador

--- app/Http/Controllers/AdminValidacionFarmaciaCompletoController.php
+++ app/Http/Controllers/AdminValidacionFarmaciaCompletoController.php
@@ -282,7 +282,7 @@
 	        |
 	        */
             $this->pre_index_html = null;
-            $this->pre_index_html = "<div class='row'>
+            $this->pre_index_html = "<div class='row '>
             <div class='col-md-12'>
             <div class='panel panel-default'>
             <div class='panel-heading'>
